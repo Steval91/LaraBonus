5b1269b5a153f0e5fa19e7363d794a2a88aecfed
Steval91 <167100080+Steval91@users.noreply.github.com>
2024-05-15 01:23:19 +0700

    Remove unused import.

--- resources/js/Pages/Error/Unauthorized.vue
+++ resources/js/Pages/Error/Unauthorized.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, usePage } from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 
 defineProps({
     message: { type: String },
